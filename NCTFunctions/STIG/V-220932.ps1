259d54372a6d56708ed8a8e64f0fd1ad79d126ef
ncterry1 <ncterry1@gmail.com>
2021-12-08 15:15:51 -0500

    adjust STIG check V-220932

--- NCTFunctions/STIG/V-220932.ps1
+++ NCTFunctions/STIG/V-220932.ps1
@@ -25,46 +25,51 @@ Function Search-V-220932 {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $false)] #User must include 'gridview' when calling function.
-        [bool] $gridview
+        [string] "STIG"
     )#End Param----------------------
+
+
+        # Set the HashTable for the target STIG
+        $V_220932 = @{
+            key = 'HKLM:\System\CurrentControlSet\Services\LanManServer\Parameters'
+            name = 'RestrictNullSessAccess'
+            expectedValue = 1
+            currentValue = $null
+        }#End V-220932 HashTable
+
+
+        # Capture target registry values, add specific value to hashtable.
+        $reg = Get-ItemProperty -Path $V_220932.key -Name $V_220932.name
+        $V_220932["currentValue"] = $reg.($V_220932.name)
+
+
+        If($V_220932.currentValue -eq 1) {
+            Write-Host ("Key: " + $V_220932.key)
+            Write-Host ("Name: " + $V_220932.name)
+            Write-Host ("Expected Value: " + $V_220932.expectedValue)
+            Write-Host ("Current Value: " + $V_220932.currentValue)
+            Write-Host -ForegroundColor Green ("STIG Compliant")
+        } Else {
+            Write-Host -ForegroundColor Red ("Not STIG Compliant")
+            Write-Host -ForegroundColor Red ("Value is set to " + $V_220932.currentValue +", not the expected value of " + $V_220932.expectedValue)
+        }#End If/Else
     
+    
+            
         #Function Tasks
         $timestamp = Get-Date -f 'yyyy-MM-dd-HHmms'
     
+
         # Get-location of this script.
         $gl = $PSScriptRoot
         $modulePath = $gl.TrimEnd("\NCTFunctions") #Moves back to main NCTSecurityModule directory.
         $resultsPath = "$modulePath\NCTData" #Directs path to the data directory. 
         If(!(Test-Path -Path $resultsPath))
 
+
         # Get local machine IPv4
         $ipv4 = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress
             
-        Write-Host("Checking for Vulnerability: V-220932")
-        # Target Registry Key
-        $key = "HKLM:\System\CurrentControlSet\Services\LanManServer\Parameters"
-        # Target Registry Key Attribute
-        $name = "RestrictNullSessAccess"
-        # STIG Compliant Value
-        $expectedValue = 1
-        # Gather local values from the Registry Key
-        $reg = Get-ItemProperty -Path $key -Name $value
-        # Narrow these values down to just the target attribute.
-        $currentValue = $reg.$name
-
-        If($currentValue -eq 1) {
-            Write-Host ("Key: $key")
-            Write-Host ("Name: $name")
-            Write-Host ("Expected Value: $expectedValue")
-            Write-Host ("Current Value: $currentValue")
-            Write-Host -ForegroundColor Green ("STIG Compliant")
-            
-        } Else {
-            Write-Host -ForegroundColor Red ("Not STIG Compliant")
-            Write-Host -ForegroundColor Red ("Value is set to $currentValue, not the expected value of $expectedValue")
-        }
-    
-    
     
         # If function ends up with a single result
         $results = ""
